f3d63424f0c52ef350c44a7d99c6920a3055623f
Anto <mdnazirulmobinahamed@gmail.com>
2023-11-06 21:15:12 +0600

    skill section modified

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
+/*$(document).ready(function(){
+    $('.project-carousel').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 5000, // Change slide every 5 seconds
+        prevArrow: '<button type="button" class="slick-prev">&#10094;</button>',
+        nextArrow: '<button type="button" class="slick-next">&#10095;</button>'
+    });
+});*/
+
 // Smooth scrolling for anchor links
 document.addEventListener('DOMContentLoaded', function () {
+    //1
     const scrollLinks = document.querySelectorAll('a.scroll-link');
 
     for (const scrollLink of scrollLinks) {
         scrollLink.addEventListener('click', smoothScroll);
     }
 
+    //2
     const scrollToTopBtn = document.getElementById('scrollToTopBtn');
 
     window.addEventListener('scroll', () => {
@@ -19,6 +32,29 @@ document.addEventListener('DOMContentLoaded', function () {
     scrollToTopBtn.addEventListener('click', () => {
         document.body.scrollTop = 0;
         document.documentElement.scrollTop = 0;
+    });
+
+    //3
+    const categories = document.querySelectorAll('.skills-navbar li');
+    const skillCategories = document.querySelectorAll('.skill-category');
+
+    categories.forEach(category => {
+        category.addEventListener('click', function() {
+            // Remove 'active' class from all categories
+            categories.forEach(cat => cat.classList.remove('active'));
+
+            // Add 'active' class to the clicked category
+            this.classList.add('active');
+
+            // Hide all skill categories
+            skillCategories.forEach(category => {
+                category.classList.remove('active');
+            });
+
+            // Show the selected skill category
+            const selectedSkillCategory = document.querySelector(`.${this.dataset.category}`);
+            selectedSkillCategory.classList.add('active');
+        });
     });
 });
 
